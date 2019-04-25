4a5a04bd0d77592af6ef5eb493b7fd7fd8e17fff
shawnping <shawnping@outlook.com>
2019-04-25 23:42:33 +0800

    v0.002 Add some variable

--- snake.asm
+++ snake.asm
@@ -3,10 +3,11 @@ assume cs:code,ss:stack,ds:data
 ;main code
 ;==================================================
 code    segment
-start:  mov ax,data  
+start:  mov ax,maindata  
         mov ds,ax
         mov ax,stack
         mov ss,ax
+        mov sp,0
 ;==================================================
 ;menu code
 ;==================================================
@@ -23,14 +24,26 @@ code    ends
 ;==================================================
 ;maindata
 ;==================================================
-data    segment
-data    ends
+maindata    segment
+    disMod      dw 4F02H,107H   ;displaymodedata
+    resola      dw 0,0
+    cellsize    dw 0            ;cell
+    winWidth    dw 0            ;cell snake size
+    widHeight   dw 0
+    boundary    dw 0,0,0,0      ;boundary coordinate
+maindata    ends
 ;==================================================
 ;main stack
 ;==================================================
 stack   segment
+    db  10000 dup(0)
 stack   ends
 ;==================================================
 ;subpro area
 ;==================================================
+subdata     segment
+    startP  dw 0                ;snake start point
+    overP   dw 0                ;snake end point
+subdata     ends
+
 end     start
