639f3edf0a9a8b42e0458c760eba481afc702dd6
shawnping <shawnping@outlook.com>
2019-04-26 22:54:35 +0800

    v0.003 add game conculate model

--- snake.asm
+++ snake.asm
@@ -1,4 +1,4 @@
-assume cs:code,ss:stack,ds:data
+assume cs:code,ss:stack,ds:maindata
 ;==================================================
 ;main code
 ;==================================================
@@ -13,7 +13,29 @@ start:  mov ax,maindata
 ;==================================================
 menu:
 
-jmp menu
+menuwait:
+
+jmp menuwait
+
+box:    mov bx,resola[2]    ;calculate the gamebox size
+        mov ax,bx
+        div cellsize
+        mov winWidth,ax
+        mov widHeight,ax
+        shl dx,1
+        mov boundary[4],dx
+        mov boundary[6],bx
+        sub boundary[6],dx
+        mov bx,boundary[6]
+        sub bx,dx
+        mov ax,resola[0]
+        sub ax,bx
+        shl ax,1
+        mov boundary[0],ax
+        add ax,bx
+        mov boundary[2],ax
+        
+
 ;==================================================
 ;game controller
 ;==================================================
@@ -26,24 +48,26 @@ code    ends
 ;==================================================
 maindata    segment
     disMod      dw 4F02H,107H   ;displaymodedata
-    resola      dw 0,0
-    cellsize    dw 0            ;cell
+    resola      dw 1280,1024
+    cellsize    dw 50           ;cell
     winWidth    dw 0            ;cell snake size
     widHeight   dw 0
     boundary    dw 0,0,0,0      ;boundary coordinate
 maindata    ends
 ;==================================================
-;main stack
-;==================================================
-stack   segment
-    db  10000 dup(0)
-stack   ends
-;==================================================
 ;subpro area
 ;==================================================
 subdata     segment
-    startP  dw 0                ;snake start point
-    overP   dw 0                ;snake end point
+    startP  dw 0,0               ;snake start point
+    overP   dw 0,0               ;snake end point
+    vector  dw 1,0               ;snake forward orientation
 subdata     ends
 
+
+;==================================================
+;main stack
+;==================================================
+stack   segment
+    db  1000 dup(0)
+stack   ends
 end     start
